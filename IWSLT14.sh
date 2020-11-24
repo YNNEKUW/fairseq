2717478fe68cf14ff5b712549c9353e22e11aa76
吳宗翰 <alliryc@login.speech>
2020-11-24 11:06:52 +0800

    .

--- IWSLT14.sh
+++ IWSLT14.sh
@@ -1,13 +1,11 @@
 python fairseq_cli_train.py \
-    data-bin/iwslt14.tokenized.de-en \
+    data-bin/bug14 \
     --arch transformer_iwslt_de_en \
     --share-decoder-input-output-embed \
     --optimizer adam \
     --adam-betas '(0.9, 0.98)' \
-    --clip-norm 0.1 \
-    --lr 5e-4 \
-    --lr-scheduler inverse_sqrt \
-    --warmup-updates 4000 \
+    --lr 8e-4 \
+    --warmup-updates 10000 \
     --dropout 0.3 \
     --weight-decay 0.0001 \
     --criterion label_smoothed_cross_entropy \
@@ -20,7 +18,7 @@ python fairseq_cli_train.py \
     --eval-bleu-print-samples \
     --best-checkpoint-metric bleu --maximize-best-checkpoint-metric \
     --num-workers 8 \
-    --save-dir /groups/alliryc/fairseq_checkpoints/transformer_iwslt_de_en_6_6_baseline
-
-
+    --save-dir /groups/alliryc/fairseq_checkpoints/transformer_iwslt_de_en_6_6_mine_baseline_v2_2_8e-4_adam_eps_1e-9_warmup_10000 \
+    --lr-scheduler inverse_sqrt \
+    --adam-eps 1e-9
 # --clip-norm 0.1 # is required when using normalized version
